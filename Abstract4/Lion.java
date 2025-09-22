86e55e251d5eccbbd16b55e2f282b7b5bc03c1b1
anushkaluniya09 <anushkaluniya2@gmail.com>
2025-09-22 14:24:18 +0530

    Delete Abstract4 directory

--- Abstract4/Lion.java
+++
@@ -1,5 +0,0 @@
-package com.Abstract.Abstract4;
-class Lion extends Animal {
-    void eat() { System.out.println("Eating meat"); }
-    void sound() { System.out.println("Roar"); }
-}
--- Abstract4/Test4.java
+++
@@ -1,8 +0,0 @@
-package com.Abstract.Abstract4;
-
-public class Test4 {
-    public static void main(String[] args) {
-        Animal a = new Lion();
-        a.live();
-    }
-}
